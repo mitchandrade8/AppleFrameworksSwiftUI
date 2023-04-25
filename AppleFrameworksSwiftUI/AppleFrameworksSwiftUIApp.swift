04435a0a3ed9b0875515ab9dcb15307e47f5da84
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-04-25 16:43:14 -0600

    Initial Commit

---
+++ AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksSwiftUIApp.swift
+//  AppleFrameworksSwiftUI
+//
+//  Created by Mitch Andrade on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
